17934c27c297951370add14d41c14d630d224ba8
Karel Nagel <nagelkarel@gmail.com>
2023-03-08 15:23:38 +0200

    daisyui

--- landing/tailwind.config.cjs
+++ landing/tailwind.config.cjs
@@ -1,10 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#0084ff",
+          secondary: "#ff5778",
+          accent: "#37cdbe",
+          neutral: "#171b1e",
+          "base-100": "#ffffff",
+          "base-content": "#000000",
+        },
+        dark: {
+          primary: "#0084ff",
+          secondary: "#ff5778",
+          accent: "#37cdbe",
+          neutral: "#171b1e",
+          "base-100": "#07090a",
+          "base-200": "#181818",
+          "base-content": "#fff",
+        },
 
-module.exports = config;
+      },
+    ],
+  },
+  plugins: [
+    require('daisyui')
+  ],
+}

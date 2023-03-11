5d6c1625acc84176014fc4627148a04c9992dfab
Karel Nagel <nagelkarel@gmail.com>
2023-03-11 14:37:35 +0200

    code blocks

--- app/src/components/Markdown.tsx
+++ app/src/components/Markdown.tsx
@@ -1,6 +1,7 @@
 import ReactMarkdown from "react-markdown";
 import { open } from "@tauri-apps/api/shell";
 import { useStore } from "../hooks/useStore";
+import { useState } from "react";
 
 export const Markdown = ({ children }: { children: string }) => {
   const textSize = useStore((s) => s.textSize);
@@ -15,9 +16,34 @@ export const Markdown = ({ children }: { children: string }) => {
             </button>
           );
         },
+        code: ({ children }) => {
+          return (
+            <code className="group relative w-full overflow-auto">
+              <Copy value={children} />
+              {children}
+            </code>
+          );
+        },
       }}
     >
       {children}
     </ReactMarkdown>
+  );
+};
+
+const Copy = ({ value }: { value: any }) => {
+  const [copied, setCopied] = useState(false);
+  const copy = () => {
+    navigator.clipboard.writeText(value);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 3000);
+  };
+  return (
+    <div
+      className="absolute top-0 right-0 cursor-pointer bg-primary bg-opacity-80 text-xs rounded-lg opacity-0 group-hover:opacity-100 duration-200 p-1"
+      onClick={copy}
+    >
+      {copied ? "Copied" : "Copy"}
+    </div>
   );
 };
